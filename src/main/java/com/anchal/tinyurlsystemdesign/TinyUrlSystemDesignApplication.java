845fd2c65924209d688edb8bb5595c42ada0493e
anchal66 <anchalavinash66@gmail.com>
2024-01-31 22:26:16 +0530

    Initial commit

---
+++ src/main/java/com/anchal/tinyurlsystemdesign/TinyUrlSystemDesignApplication.java
@@ -0,0 +1,13 @@
+package com.anchal.tinyurlsystemdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlSystemDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyUrlSystemDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anchal/tinyurlsystemdesign/TinyUrlSystemDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anchal.tinyurlsystemdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyUrlSystemDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
